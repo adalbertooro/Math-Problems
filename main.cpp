a16c6cc3719b867c518c3467f360ae4bf76befb3
Adalberto Orozco <z1885975@students.niu.edu>
2022-12-09 15:40:15 -0600

    Add function calls to main to access formula class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include "formula.h"
 
 int main() {
+    formula frm;
+    int var = 0;
+    frm.print();
+
+    std::cin >> var;
+    
+    frm.find(var);
+    
     return 0;
 }
