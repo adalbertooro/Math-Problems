06f85fd36ec22241e21ada30a3daa6007630f02e
Adalberto Orozco <adalbertooro@gmail.com>
2022-12-10 14:44:55 -0600

    finished math for collatz // math has no format right now

--- formula.h
+++ formula.h
@@ -1,107 +1,52 @@
 #ifndef FORMULA_H
 #define FORMULA_H
 
-
 #include <iostream>
+
 class formula {
-public:
-    void find(int);
+    public:
+        double add(double, double);
+        double subtract(double, double);
+        double multiply(double, double);
+        double divide(double, double);
+        int collatzConjecture(int);
 
-    int add(int, int);
-    int subtract(int, int);
-    int multiply(int, int);
-    int divide(int, int);
-    int collatzConjecture(int);
-
-private:
-
+    private:
 };
 
-void formula::find(int var) {
-    formula frm;
-    int total = 0;
-    if(var == 1) {
-        int a = 0;
-        int b = 0;
-        std::cout << "\npick two numbers to add together.\n";
-        std::cout << "First Number: ";
-        std::cin >> a;
-        std::cout << "Second Number: ";
-        std::cin >> b;
-        total = frm.add(a, b);
-        std::cout << "The sum of the " << a << " + " << b << " = " << total; 
-        std::cout << "\n";
-    } else if(var == 2) {
-        int a = 0;
-        int b = 0;
-        std::cout << "\npick two numbers to subtract together.\n";
-        std::cout << "First Number: ";
-        std::cin >> a;
-        std::cout << "Second Number: ";
-        std::cin >> b;
-        total = frm.subtract(a, b);
-        std::cout << "The sum of the " << a << " - " << b << " = " << total; 
-        std::cout << "\n";
-    } else if(var == 3) {
-        int a = 0;
-        int b = 0;
-        std::cout << "\npick two numbers to multiply together.\n";
-        std::cout << "First Number: ";
-        std::cin >> a;
-        std::cout << "Second Number: ";
-        std::cin >> b;
-        total = frm.multiply(a, b);
-        std::cout << "The sum of the " << a << " * " << b << " = " << total; 
-        std::cout << "\n";
-    } else if(var == 4) {
-        int a = 0;
-        int b = 0;
-        std::cout << "\npick two numbers to divide together.\n";
-        std::cout << "First Number: ";
-        std::cin >> a;
-        std::cout << "Second Number: ";
-        std::cin >> b;
-        total = frm.divide(a, b);
-        std::cout << "The sum of the " << a << " / " << b << " = " << total; 
-        std::cout << "\n";
-    } else if(var == 5) {
-        int x = 0;
-        std::cout << "\nPick a number for the Coolatz Conjecture.\n";
-        std::cout << "Number: ";
-        std::cin >> x;
-        total = frm.collatzConjecture(x);
-        std::cout << total << '\n';
-    }
-}
-
-int formula::add(int a, int b) {
-    int sum = a + b;
+double formula::add(double a, double b) {
+    double sum = a + b;
     return sum;
 }
 
-int formula::subtract(int a, int b) {
-    int sum = a - b;
+double formula::subtract(double a, double b) {
+    double sum = a - b;
     return sum;
 }
 
-int formula::multiply(int a, int b) {
-    int sum = a * b;
+double formula::multiply(double a, double b) {
+    double sum = a * b;
     return sum;
 }
 
-int formula::divide(int a, int b) {
-    int sum = a / b;
+double formula::divide(double a, double b) {
+    double sum = a / b;
     return sum;
 }
 int formula::collatzConjecture(int x) {
     formula frm;
+    int total = x;
     int sum = 0;
-
-    if(x % 2 == 0) {
-        std::cout << "Even\n";
-    } else {
-        sum = frm.multiply(x, 3);
-        sum = frm.add(sum, 1);
+    while(total != 1) {
+        if(total % 2 == 0) {
+            sum = total;
+            total = frm.divide(total, 2);
+            std::cout << total <<"\n";
+        } else {
+            total = frm.multiply(total, 3);
+            total = frm.add(total, 1);
+            std::cout << total <<"\n";
+        }
     }
     return sum;
 }
