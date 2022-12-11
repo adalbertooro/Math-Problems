afab505cd05bea9124ed84fd0ce35cad89eff0b2
Adalberto Orozco <adalbertooro@gmail.com>
2022-12-10 19:37:13 -0600

    finished print statment for collatz conjecture

--- printing.h
+++ printing.h
@@ -3,7 +3,7 @@
 
 #include "formula.h"
 
-#include <iostream>
+#include<iomanip>
 
 class printing {
     public:
@@ -13,6 +13,7 @@ class printing {
         void printSubtract(double, double, double);
         void printMultiply(double, double, double);
         void printDivide(double, double, double);
+        void printCollatz(std::vector<int>);
     private:
 
 };
@@ -32,6 +33,7 @@ void printing::find(int var) {
     printing prt;
     std::cout.precision(5);
     double total = 0.0;
+    std::vector<int> hold;
     if(var == 1) {
         double a = 0;
         double b = 0;
@@ -73,12 +75,12 @@ void printing::find(int var) {
         total = frm.divide(a, b);
         prt.printDivide(a, b, total);
     } else if(var == 5) {
-        int x = 0;
+        double x = 0;
         std::cout << "\nPick a number for the Coolatz Conjecture.\n";
         std::cout << "Number: ";
         std::cin >> x;
-        total = frm.collatzConjecture(x);
-        std::cout << total << '\n';
+        hold = frm.collatzConjecture(x);
+        prt.printCollatz(hold);
     }
 }
 
@@ -100,6 +102,20 @@ void printing::printMultiply(double a, double b, double total) {
 void printing::printDivide(double a, double b, double total) {
     std::cout << total << " = " << a << " / " << b; 
     std::cout << "\n";
+}
+
+void printing::printCollatz(std::vector<int> info) {
+    for(long unsigned int i = 0; i < info.size(); ++i) {
+        if(info.at(i) == 1) {
+            std::cout << std::setw(4) << info.at(i - 2) << " = " << info.at(i) << " * 3 + 1" << '\n';
+            break;
+        }
+        if(info.at(i) % 2 != 0) {
+            std::cout << std::setw(4) << info.at(i) << " = " << info.at(i + 1) << " * 3 + 1" << '\n';
+        } else {
+            std::cout << std::setw(4) << info.at(i) << " = " << info.at(i + 1) << " / 2" << '\n';
+        }
+    }
 }
 
 #endif
